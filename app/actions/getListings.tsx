5a8a79f09993d7cc563e94deb92e373c1180dd1c
Tyler <tyler.le@edu.sait.ca>
2023-07-24 17:11:05 -0600

    Fixes listing compile error

--- app/actions/getListings.tsx
+++ app/actions/getListings.tsx
@@ -1,6 +1,5 @@
 import prisma from "@/app/libs/prismadb"
 
-
 export default async function getListings() {
     try {
         const listings = await prisma.listing.findMany({
@@ -9,7 +8,12 @@ export default async function getListings() {
             }
         });
 
-        return listings;
+        const safeListings = listings.map((listing) => ({
+            ...listing,
+            createdAt: listing.createdAt.toISOString(),
+        }))
+
+        return safeListings;
     } catch (error: any) {
         throw new Error(error)
     }
