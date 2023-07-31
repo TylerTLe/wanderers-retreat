fc940eef959feacda85f2ad7d56985f93051456f
Tyler <tyler.le@edu.sait.ca>
2023-07-31 14:22:04 -0600

    redeploys

--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -6,42 +6,36 @@ import getReservations from "../actions/getReservations";
 import TripsClient from "./TripsClient";
 
 const TripsPage = async () => {
-    const currentUser = await getCurrentUser();
+  const currentUser = await getCurrentUser();
 
-    if (!currentUser) {
-        return (
-            <ClientOnly>
-                <EmptyState 
-                title="Unauthorized"
-                subtitle="Please Login"
-                />
-            </ClientOnly>
-        )
-    }
-
-    const reservations = await getReservations({
-        userId: currentUser.id
-    });
-
-    if (reservations.length === 0) {
-        return (
-            <ClientOnly>
-            <EmptyState 
-            title="No trips found"
-            subtitle="Looks like you haven't reserved any trips."
-            />
-        </ClientOnly>
-        ) 
-    }
-
+  if (!currentUser) {
     return (
-        <ClientOnly>
-            <TripsClient 
-            reservations={reservations}
-            currentUser={currentUser}
-            />
-        </ClientOnly>
-    )
-}
+      <ClientOnly>
+        <EmptyState title="Unauthorized" subtitle="Please Login" />
+      </ClientOnly>
+    );
+  }
+
+  const reservations = await getReservations({
+    userId: currentUser.id,
+  });
+
+  if (reservations.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No trips found"
+          subtitle="Looks like you haven't reserved any trips."
+        />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <TripsClient reservations={reservations} currentUser={currentUser} />
+    </ClientOnly>
+  );
+};
 
 export default TripsPage;
